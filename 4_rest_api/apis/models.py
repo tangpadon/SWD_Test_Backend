66eba1f708a87a50e14df1d2525c2c8ad123c331
tangpadon <txng.rn@gmail.com>
2026-09-13 19:59:53 +0700

    Update: models.py for migrate

--- 4_rest_api/apis/models.py
+++ 4_rest_api/apis/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Gender(models.TextChoices):
+    MALE = 'M', 'Male'
+    FEMALE = 'F', 'Female'
+
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    abbreviation = models.CharField(max_length=20)
+    address = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Classroom(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='classrooms')
+    grade = models.CharField(max_length=50)
+    room = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.school.name} - {self.grade}/{self.room}"
+
+class Teacher(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=Gender.choices)
+    classrooms = models.ManyToManyField(Classroom, related_name='teachers', blank=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+class Student(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=Gender.choices)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
