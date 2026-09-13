c5fc728c49270dcdc7b6abd4aab56c58ed1a533b
tangpadon <txng.rn@gmail.com>
2026-09-13 20:45:32 +0700

    Fix: Recursion

--- 4_rest_api/apis/serializers.py
+++ 4_rest_api/apis/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 # code here
 from apis.models import School, Classroom, Teacher, Student
 
+
 class SchoolSerializer(serializers.ModelSerializer):
     classroom_count = serializers.SerializerMethodField()
     teacher_count = serializers.SerializerMethodField()
@@ -11,7 +12,7 @@ class SchoolSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = School
-        fields = ['id', 'name', 'abbreviation', 'address', 'classroom_count', 'teacher_count', 'student_count']
+        ##fields = ['id', 'name', 'abbreviation', 'address', 'classroom_count', 'teacher_count', 'student_count']
         fields = '__all__'
 
     def get_classroom_count(self, obj):
@@ -23,12 +24,14 @@ class SchoolSerializer(serializers.ModelSerializer):
     def get_student_count(self, obj):
         return Student.objects.filter(classroom__school=obj).count()
 
+
 class StudentSerializer(serializers.ModelSerializer):
-    classroom = serializers.PrimaryKeyRelatedField(queryset=Classroom.objects.all())
+    ##classroom = serializers.PrimaryKeyRelatedField(queryset=Classroom.objects.all())
 
     class Meta:
         model = Student
         fields = '__all__'
+
 
 class ClassroomSerializer(serializers.ModelSerializer):
     teachers = serializers.PrimaryKeyRelatedField(queryset=Teacher.objects.all(), many=True, required=False)
@@ -39,9 +42,10 @@ class ClassroomSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        data['teachers'] = TeacherSerializer(instance.teachers, many=True).data
+        data['teachers'] = TeacherBriefSerializer(instance.teachers, many=True).data
         data['students'] = StudentSerializer(instance.students, many=True).data
         return data
+
 
 class TeacherSerializer(serializers.ModelSerializer):
     classrooms = serializers.PrimaryKeyRelatedField(queryset=Classroom.objects.all(), many=True, required=False)
@@ -52,5 +56,19 @@ class TeacherSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        data['classrooms'] = ClassroomSerializer(instance.classrooms, many=True).data
+        data['classrooms'] = ClassroomBriefSerializer(instance.classrooms, many=True).data
         return data
+
+
+class ClassroomBriefSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classroom
+        fields = ['id', 'school', 'grade', 'room']
+
+
+class TeacherBriefSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = ['id', 'firstname', 'lastname', 'gender']
+
+
