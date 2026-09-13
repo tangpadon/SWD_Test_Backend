76b33d07af7bcf9f93eff1e50f6d50eb6e22146f
tangpadon <txng.rn@gmail.com>
2026-09-13 20:08:02 +0700

    Update filters.py

--- 4_rest_api/apis/filters.py
+++ 4_rest_api/apis/filters.py
@@ -2,3 +2,40 @@ from django_filters import FilterSet, filters
 
 
 # code here
+from apis.models import School, Classroom, Teacher, Student
+
+class SchoolFilter(FilterSet):
+    class Meta:
+        model = School
+        fields = {
+            'name': ['exact'],
+        }
+
+class ClassroomFilter(FilterSet):
+    class Meta:
+        model = Classroom
+        fields = {
+            'school': ['exact'],
+        }
+
+class TeacherFilter(FilterSet):
+    class Meta:
+        model = Teacher
+        fields = {
+            'classrooms_school': ['exact'],
+            'classrooms' : ['exact'],
+            'firstname': ['exact'],
+            'lastname': ['exact'],
+            'gener': ['exact'],
+        }
+
+class StudentFilter(FilterSet):
+    class Meta:
+        model = Student
+        fields = {
+            'classroom_school': ['exact'],
+            'classroom' : ['exact'],
+            'firstname': ['exact'],
+            'lastname': ['exact'],
+            'gender': ['exact'],
+        }
